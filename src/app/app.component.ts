9914b29c80ee48544e4408acbb141cf3fd3a2c04
chandru-personal <mailtochandrupn@gmail.com>
2020-05-28 20:23:36 -0600

    The Component Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app',
-    template: `<h1>My App</h1>`
+    template: `
+        <h1>My App</h1>
+        <p>Keeping track of media I want to watch.</p>
+    `
 })
 export class AppComponent {}
