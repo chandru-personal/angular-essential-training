f3bf7cd90a926e54647ef5c24aeedec239462199
chandru-personal <mailtochandrupn@gmail.com>
2020-05-28 20:27:30 -0600

    The Component TemplateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app',
-    template: `
-        <h1>My App</h1>
-        <p>Keeping track of media I want to watch.</p>
-    `
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {}
